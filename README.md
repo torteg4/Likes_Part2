62afb2f560bccc7384f1d86fda1f526a1665d64e
Tiffany <tiffanyortega29@gmail.com>
2022-05-25 20:02:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello world! 
+
+This is a readme for my first project uploaded!
---
+++ script.js
@@ -0,0 +1,28 @@
+var count1 = 9
+var neilElement = document.querySelector(".neillikes");
+
+function add1(){
+    count1++;
+    neilElement.innerText = count1 + " like(s)";
+    console.log(count1);
+}
+// Neil count function
+
+var count2 = 12
+var nicholeElement = document.querySelector(".nicholelikes");
+
+function add2(){
+    count2++;
+    nicholeElement.innerText = count2 + " like(s)";
+    // console.log(count2);
+}
+// Nichole count function
+
+var count3 = 9
+var jimElement = document.querySelector(".jimlikes");
+
+function add3(){
+    count3++;
+    jimElement.innerText = count3 + " like(s)";
+}
+// Jim count function
